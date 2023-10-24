e79b3e104ec86ca40d65dc7801ace15096fceed2
BrennoEscobar <106408359+BrennoEscobar@users.noreply.github.com>
2023-10-23 21:05:04 -0300

    Add files via upload

---
+++ sistema-cadastro/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'sistema-cadastro';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+
+?>
